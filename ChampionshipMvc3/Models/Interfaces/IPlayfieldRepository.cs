d36d8bfa873679a0f6c237544db243ecdc6b20e2
vpp090 <vasil.petrov090@gmail.com>
2014-03-23 14:23:36 +0200

    Added interfaces for Playfield and Schedule Repositories.

---
+++ ChampionshipMvc3/Models/Interfaces/IPlayfieldRepository.cs
@@ -0,0 +1,15 @@
+﻿using ChampionshipMvc3.Models.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChampionshipMvc3.Models.Interfaces
+{
+    public interface IPlayfieldRepository
+    {
+        void AddNewPlayfield(Playfield playfield);
+        Playfield GetModel();
+        void SaveChanges();
+    }
+}
