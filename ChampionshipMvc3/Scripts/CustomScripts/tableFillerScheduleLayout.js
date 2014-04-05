62e6cccc21788ab61e2f81ac9985d0449f466a48
Zlatinsz <Zlatin_sz@yahoo.com>
2014-04-05 19:10:35 +0300

    Added regular expression for the matching of the string

--- ChampionshipMvc3/Scripts/CustomScripts/tableFillerScheduleLayout.js
+++ ChampionshipMvc3/Scripts/CustomScripts/tableFillerScheduleLayout.js
@@ -1,12 +1,22 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function () {
+    var stringToBeReplaced;
     $('#scheduleTableID tr').each(function () {
+        var $td = $(this).find("td");
         var $tds = $(this).find("td").text();
-     //   alert($tds);
-        if ($tds == "False") {
+        var checkForFalse = RegExp(/false/i);
+
+        if ($tds.match(checkForFalse)) {
             $(this).css('background-color', 'red');
+            $(this).html($(this).html().replace(/False/gi, function (match, tag) {
+                return (tag === "a") ? match : "";
+            }));
+            
         }
-        else if ($tds == "True") {
+        else if (!$tds.match(checkForFalse)) {
             $(this).css('background-color', 'green')
+            $(this).html($(this).html().replace(/True/gi, function (match, tag) {
+                return (tag === "a") ? match : "";
+            }));
         }
 
     });
