45df2638433f0ed946ce59fe1978aa0317ce1019
Zlatinsz <Zlatin_sz@yahoo.com>
2014-04-05 14:17:49 +0300

    Added the table color filler the javascript responsible for that is "tableFillerScheduleLayuot.js"

---
+++ ChampionshipMvc3/Scripts/CustomScripts/tableFillerScheduleLayout.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function() {
+    $('#scheduleTableID tr').each(function () {
+        var $tds = $(this).find("td").text();
+     //   alert($tds);
+        if ($tds == "False") {
+            $(this).css('background-color', 'red');
+        }
+        else if ($tds == "True") {
+            $(this).css('background-color', 'green')
+        }
+
+    });
+});
