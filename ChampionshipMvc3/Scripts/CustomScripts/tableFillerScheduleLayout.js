5c401b4a2b0895d1bdecd357585a971c3f98f423
Zlatinsz <Zlatin_sz@yahoo.com>
2014-04-06 16:10:08 +0300

    BIG DADDY MERGE

--- ChampionshipMvc3/Scripts/CustomScripts/tableFillerScheduleLayout.js
+++ ChampionshipMvc3/Scripts/CustomScripts/tableFillerScheduleLayout.js
@@ -8,19 +8,21 @@
 
     $('#scheduleTableID tr').each(function () {
         var $td = $(this).find("td");
+        var $tda = $td.find("a");
         var $tds = $(this).find("td").text();
         var checkForFalse = RegExp(/false/i);
-        
+     
         if ($tds.match(checkForFalse)) {
             $(this).css("position", "relative");
             $(this).css('background-color', 'red');
             $(this).css('border', '0px');
             $(this).css("text-align", "center");
             $(this).css('vertical-align', 'middle');
-
+            
             $(this).html($(this).html().replace(/False/gi, function (match, tag) {
                 return (tag === "a") ? match : "";
             }));
+        
         }
         else if (!$tds.match(checkForFalse)) {
             $(this).css("position", "relative");
@@ -32,6 +34,9 @@
             $(this).html($(this).html().replace(/True/gi, function (match, tag) {
                 return (tag === "a") ? match : "";
             }));
+    
         }
     });
+
+    
 });
